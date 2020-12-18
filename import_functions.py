976fc5e65644e1ad55327697091df6a8e95b6d41
manojkumar00158 <74287117+manojkumar00158@users.noreply.github.com>
2020-12-18 08:58:21 +0530

    Add files via upload

---
+++ import_functions.py
@@ -0,0 +1,27 @@
+import function_methods2
+
+print(dir(function_methods2))
+a = 2
+b = 6
+print(function_methods2.addition(a,b))
+
+a = 5
+b = 10
+print(function_methods2.multiplication(a,b))
+
+a = 15
+b = 3
+print(function_methods2.division(a,b))
+
+n = 6
+print(function_methods2.factorial(n))
+
+print(function_methods2.substraction(a=12,b=3))
+
+c = 7
+function_methods2.even_odd(c)
+
+
+
+  
+
